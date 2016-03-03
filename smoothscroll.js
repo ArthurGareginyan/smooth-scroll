2afd0382a656640bae2468ceaefbaa366125cdb3
Arthur Gareginyan <arthurgareginyan@gmail.com>
2016-03-03 12:47:51 +0300

    Updating the smoothscroll.js to version 1.2

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,23 +1,23 @@
 /*
- Smooth scroll v1.1 | @agareginyan | GPL v3 Licensed
+ Smooth scroll v1.2 | @agareginyan | GPL v3 Licensed
 */
 
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function() {
 
-    $('#back-to-top').hide();
+    jQuery('#back-to-top').hide();
 
-	$(window).scroll(function() {
-        if ($(this).scrollTop() < 200) {
-			$('#back-to-top') .fadeOut();
-        } else {
-			$('#back-to-top') .fadeIn();
-        }
+    jQuery(window).scroll(function() {
+                     if (jQuery(this).scrollTop() < 200) {
+                        jQuery('#back-to-top') .fadeOut();
+                     } else {
+                        jQuery('#back-to-top') .fadeIn();
+                     }
     });
 
-	$('#back-to-top').on('click', function() {
-		$('html, body').animate({scrollTop:0}, 'fast');
-		return false;
+    jQuery('#back-to-top').on('click', function() {
+                         jQuery('html, body').animate({scrollTop:0}, 'fast');
+                         return false;
     });
 
 });
